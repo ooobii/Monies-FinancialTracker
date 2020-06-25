cd502e078fd5cc802be6ca73bc346ad071dc8a20
Matthew <60083197+ooobii@users.noreply.github.com>
2020-06-25 14:04:55 -0400

    Add Entity; Create ApiDbContext class

---
+++ FinancialTracker_Svc/Models/ApiDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FinancialTracker_Svc.Models
+{
+    public class ApiDbContext : DbContext
+    {
+    }
+}
